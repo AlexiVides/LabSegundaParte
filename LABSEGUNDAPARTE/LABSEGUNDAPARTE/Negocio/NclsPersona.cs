16f3e03cc4f1180ead053627007914462aa7248e
Nestor2021 <87671739+Nestor2021@users.noreply.github.com>
2021-08-30 16:56:44 -0600

    Clase VISTA

--- LABSEGUNDAPARTE/LABSEGUNDAPARTE/Negocio/NclsPersona.cs
+++ LABSEGUNDAPARTE/LABSEGUNDAPARTE/Negocio/NclsPersona.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LABSEGUNDAPARTE.Negocio
 {
-    class NclsPersona
+    class NClsPersona
     {
         public String calcularIMC(Persona persona) 
         {
--- LABSEGUNDAPARTE/LABSEGUNDAPARTE/Program.cs
+++ LABSEGUNDAPARTE/LABSEGUNDAPARTE/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LABSEGUNDAPARTE.Entidades;
+using LABSEGUNDAPARTE.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,33 @@ namespace LABSEGUNDAPARTE
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("!!!!!!Escribe tus Datos!!!!!!! ");
+            Console.WriteLine("Escribe tu Nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu Edad: ");
+            int edad =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Escribe tu Sexo: ");
+            String sexo = Console.ReadLine();
+            Console.WriteLine("Escribe tu Peso: ");
+            Double peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe tu Altura: ");
+            Double altura = Convert.ToDouble(Console.ReadLine());
+
+            Persona persona = new Persona(nombre,edad,sexo,peso,altura);
+
+            Persona persona1 = new Persona(nombre, edad, sexo);
+
+            NClsPersona nclsPersona = new NClsPersona();
+            Console.WriteLine("-------------------------------- ");
+            //CALCULAR IMC
+            Console.WriteLine(nclsPersona.calcularIMC(persona));
+            //MAYOR O MENOR
+            Console.WriteLine(nclsPersona.esMayorDeEdad(persona));
+
+
+            Console.WriteLine(persona.ToString());
+            Console.ReadLine();
+
 
         }
     }
