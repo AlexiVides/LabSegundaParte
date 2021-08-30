e44370b8c134b4c3ae1c36ab2c0f3747236effac
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2021-08-30 16:13:35 -0600

    Negocios

--- LABSEGUNDAPARTE/LABSEGUNDAPARTE/Entidades/Persona.cs
+++ LABSEGUNDAPARTE/LABSEGUNDAPARTE/Entidades/Persona.cs
@@ -15,33 +15,40 @@ namespace LABSEGUNDAPARTE.Entidades
         private Double altura;
         private Double totalpeso;
 
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
+        public double Totalpeso { get => totalpeso; set => totalpeso = value; }
+
         public Persona() { }
 
         public Persona(string nombre, int edad, string sexo)
         {
-            this.nombre = nombre;
-            this.edad = edad;
-            this.sexo = sexo;
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Sexo = sexo;
         }
 
         public Persona(string nombre, int edad, string sexo, double peso, double altura)
         {
-            this.nombre = nombre;
-            this.edad = edad;
-            this.sexo = sexo;
-            this.peso = peso;
-            this.altura = altura;
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Sexo = sexo;
+            this.Peso = peso;
+            this.Altura = altura;
         }
 
         public Persona(double peso, double altura)
         {
-            this.peso = peso;
-            this.altura = altura;
+            this.Peso = peso;
+            this.Altura = altura;
         }
 
         public override string ToString()
         {
-            return "Nomnbre " + nombre +"Edad " + edad + "Sexo "+sexo + "Peso " + peso + "Altura " + altura;
+            return "Nomnbre " + Nombre +"Edad " + Edad + "Sexo "+Sexo + "Peso " + Peso + "Altura " + Altura;
         }
     }
 }
--- LABSEGUNDAPARTE/LABSEGUNDAPARTE/Negocio/NclsPersona.cs
+++ LABSEGUNDAPARTE/LABSEGUNDAPARTE/Negocio/NclsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LABSEGUNDAPARTE.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace LABSEGUNDAPARTE.Negocio
 {
     class NclsPersona
     {
+        public String calcularIMC(Persona persona) 
+        {
+            persona.Totalpeso = (persona.Peso / Math.Pow(persona.Altura, 2));
+
+            if (persona.Totalpeso < 20)
+            {
+                return "Peso ideal";
+            }
+            else
+            {
+                return "Tiene sobrepeso";
+            }
+        }
+
+        public string esMayorDeEdad(Persona persona)
+        {
+            if (persona.Edad > 18)
+            {
+                return "Es mayor de Edad";
+            }
+            else
+            {
+                return "Es menor de edad";
+            }
+        }
     }
 }
